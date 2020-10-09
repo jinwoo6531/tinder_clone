a5e56d410c966852414fc67cd569a5a4bb0da4ca
jinwoo <jintakk6531@gmail.com>
2020-10-09 13:41:19 +0900

    Updates

---
+++ precache-manifest.9c004adf90e8677a79ac29c0f5c2247c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c786da410f19eb16d90ca3e0a30614a",
+    "url": "/tinder_clone/index.html"
+  },
+  {
+    "revision": "514510f96140fd50563a",
+    "url": "/tinder_clone/static/css/main.1bc5b1b9.chunk.css"
+  },
+  {
+    "revision": "21ae2214e8dabf5ffd67",
+    "url": "/tinder_clone/static/js/2.fe09a335.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/tinder_clone/static/js/2.fe09a335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514510f96140fd50563a",
+    "url": "/tinder_clone/static/js/main.3b8a4c0e.chunk.js"
+  },
+  {
+    "revision": "667508f286b7987a87d6",
+    "url": "/tinder_clone/static/js/runtime-main.9cce69e3.js"
+  }
+]);
